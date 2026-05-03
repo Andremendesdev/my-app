3f583f1e33d5e40b03b5fae1302cd8b83b51aa7d
andre <andremendespr@gmail.com>
2026-05-03 00:06:43 -0300

    header add

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cartpage = () => {
+  return <div>page</div>;
+};
+
+export default Cartpage;
---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <header className="bg-linear-to-r from-indigo-600 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... py-5">
+      <div>
+        {/*LeftBar*/}
+        <Container className="flex items-center justify-between gap-7 text-lg">
+          <HeaderMenu />
+          <Logo />
+          <div>RightBar</div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
